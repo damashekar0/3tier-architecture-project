e83565d7848bc9c610a594fceb9833a50c63bd5f
damashekar0 <damashekar0@gmail.com>
2026-03-14 13:46:30 +0530

    Add files via upload

---
+++ 3-tier-app-deployment-aws/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'my3tierdb.cvgka4wg0f8h.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'shekar7093',
+    DB_DATABASE : 'webappdb'
+});
